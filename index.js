981e237e61f945d38a28b8b067ffd4940848c020
Brian Diaz <bidiaz101@gmail.com>
2021-08-14 12:54:46 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -8,9 +8,30 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCasedArray = tutorials.map (tutorial => {
+
+  const firstLetter = tutorial.charAt(0);
+  const firstLetterCapitalized = firstLetter.toUpperCase();
+  let tutorialCapitalized = tutorial.replace(`${firstLetter}`, `${firstLetterCapitalized}`);
+
+  for(const index in tutorialCapitalized) {
+    if(tutorialCapitalized[index] === ' ') {
+      let lastLetterBeforeSpace;
+      let firstLetterAfterSpace;
+      let firstLetterAfterSpaceCapitalized;
+      let indexNum = parseInt(index);
+      lastLetterBeforeSpace = tutorialCapitalized[indexNum-1];
+      firstLetterAfterSpace = tutorialCapitalized[indexNum+1];
+      firstLetterAfterSpaceCapitalized = firstLetterAfterSpace.toUpperCase();
+      tutorialCapitalized = tutorialCapitalized.replace(`${lastLetterBeforeSpace} ${firstLetterAfterSpace}`, `${lastLetterBeforeSpace} ${firstLetterAfterSpaceCapitalized}`);
+    };
+  };
+  return tutorialCapitalized;
+});
+
+const titleCased = tutorials => {
+  return titleCasedArray;
+};
